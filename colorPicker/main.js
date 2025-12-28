d9f01fb220c5133c78bdf74e140a271d661bbe51
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2025-12-28 21:23:44 +0600

    some add javaScript

--- colorPicker/main.js
+++ colorPicker/main.js
@@ -7,19 +7,7 @@ const copy_hsl = document.querySelector("#copy_hsl");
 const folder = document.querySelector(".folder");
 const close_folder = document.querySelector(".close_folder");
 const pickColor = document.querySelector("#pickColor");
-
-
-const activateEyeDroper = async () => {
-    try {
-        const eyeDropper = new EyeDropper();
-        const result = await eyeDropper.open();
-        const color = result.sRGBHex;
-        console.log(color)
-        
-    } catch (error) {
-        console.error(error)
-    }
-}
+const LOCAL_STORAGE_KEY = "pickedColor";
 
 //pick color action
 pickColor.addEventListener("click", () => {
@@ -33,3 +21,41 @@ pickColor.addEventListener("click", () => {
         console.log("faild to pick color");
     }
 })
+
+//activate idroper
+const activateEyeDroper = async () => {
+    try {
+        const eyeDropper = new EyeDropper();
+        const { sRGBHex } = await eyeDropper.open();
+        navigator.clipboard.writeText(sRGBHex)
+        console.log(sRGBHex)
+        const rgb = hexToRGB(sRGBHex);
+        console.log(rgb);
+        const hsl = hexToHsl(sRGBHex);
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// convert hex to rgb
+function hexToRGB(hex){
+    hex = hex.replace("#","");
+
+    // seprate hex ff bb 00 like this use substring(start,end); or convert hex to decimal use parseInt(any calculation)
+    const r = parseInt(hex.substring(0,2),16);
+    const g = parseInt(hex.substring(2,4),16);
+    const b = parseInt(hex.substring(4,6),16);
+
+    return `rgb(${r},${g},${b})`;
+}
+
+// convert hex to hsl
+function hexToHsl(hex){
+    hex = hex.replace("#","");
+    const r = parseInt(hex.substring(0,2),16) / 255;
+    const g = parseInt(hex.substring(2,4),16) / 255;
+    const b = parseInt(hex.substring(4,6),16) / 255;
+
+    
+}
