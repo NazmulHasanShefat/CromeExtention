ca01f5327053e4d1ef3dd76b956576c14e4e75c2
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2025-12-29 17:19:57 +0600

    fix some bug

--- colorPicker/main.js
+++ colorPicker/main.js
@@ -14,7 +14,7 @@ pickColor.addEventListener("click", () => {
     try {
         if (window.EyeDropper) {
             activateEyeDroper();
-        }else{
+        } else {
             alert("eyedropper not support your browser please try on Google Chrome or microsoft Edge or Brave browser ")
         }
     } catch (error) {
@@ -38,7 +38,7 @@ const activateEyeDroper = async () => {
                 id: myColor.length,
                 rgbColor: rgb,
                 hexColor: sRGBHex,
-                hslColor: hsl 
+                hslColor: hsl
             }
             // renderUIcolor()
         )
@@ -51,47 +51,47 @@ const activateEyeDroper = async () => {
 }
 
 // convert hex to rgb
-function hexToRGB(hex){
-    hex = hex.replace("#","");
+function hexToRGB(hex) {
+    hex = hex.replace("#", "");
 
     // seprate hex ff bb 00 like this use substring(start,end); or convert hex to decimal use parseInt(any calculation)
-    const r = parseInt(hex.substring(0,2),16);
-    const g = parseInt(hex.substring(2,4),16);
-    const b = parseInt(hex.substring(4,6),16);
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
 
     return `rgb(${r},${g},${b})`;
 }
 
 // convert hex to hsl
-function hexToHsl(hex){
-    hex = hex.replace("#","");
-    const r = parseInt(hex.substring(0,2),16) / 255;
-    const g = parseInt(hex.substring(2,4),16) / 255;
-    const b = parseInt(hex.substring(4,6),16) / 255;
+function hexToHsl(hex) {
+    hex = hex.replace("#", "");
+    const r = parseInt(hex.substring(0, 2), 16) / 255;
+    const g = parseInt(hex.substring(2, 4), 16) / 255;
+    const b = parseInt(hex.substring(4, 6), 16) / 255;
 
     // find max or min over all rgb 
     const max = Math.max(r, g, b);
     const min = Math.min(r, g, b);
 
     let h, s, l = (max + min) / 2;
-    if(max === min){
+    if (max === min) {
         h = s = 0;
-    }else{
+    } else {
         const d = max - min;
         s = 1 > 0.5 ? d / (2 - max - min) : d / (max + min);
 
-        switch(max){
-           case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-           case g: h = (b - r) / d + 2; break;
-           case b: h = (r - g) / d + 4; break;
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
         }
         h *= 60;
-    }   
+    }
     return `hsl(${Math.round(h)}, ${Math.round(s * 100)}%, ${Math.round(l * 100)}%)`;
 }
 
 //display as folder all colors
-function renderUIcolor(){
+function renderUIcolor() {
     const container = document.querySelector(".card");
     container.innerHTML = "";
     myColor.forEach(color => {
@@ -117,16 +117,17 @@ function renderUIcolor(){
     const close_folder = document.querySelectorAll(".close_folder");
     const folder = document.querySelectorAll(".folder");
     close_folder.forEach(delete_btn => {
-        delete_btn.addEventListener("click",(e)=>{
+        delete_btn.addEventListener("click", (e) => {
+            e.stopPropagation();
             removeSingleColor(e)
         })
     });
     folder.forEach(openFolder => {
-        openFolder.addEventListener("click", (e)=>{
+        openFolder.addEventListener("click", (e) => {
             toggleColorModal(e)
         })
     })
-    
+
 }
 
 // remove color from local storage
@@ -145,46 +146,47 @@ function removeSingleColor(e) {
 }
 
 // toggle colors modal
-function toggleColorModal(e){
+function toggleColorModal(e) {
+    console.log(e.target.firstElementChild)
     const rgb = e.target.dataset.rgb;
     const hsl = e.target.dataset.hsl;
     const hex = e.target.dataset.hex;
     console.log(rgb);
     const div = document.createElement("div")
     div.innerHTML = `<div class="drop_shadow"></div>
-    <div class="popup">
-        <div class="close">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
-                    fill="#0F1729" />
-            </svg>
-        </div>
-        <div class="cp_btn_container"> <button type="button" class="pick_button" id="copy_rgb">copy rgba</button></div>
-        <div class="p_color_box">
-            <span>${rgb}</span>
-        </div>
-        <div class="cp_btn_container"> <button type="button" class="pick_button" id="copy_hsl">copy hsl</button></div>
-        <div class="p_color_box">
-            <span>${hsl}</span>
-        </div>
-        <div class="cp_btn_container"> <button type="button" class="pick_button" id="copy_hex">copy hex</button></div>
-        <div class="p_color_box">
-            <span>${hex}</span>
-        </div>
-        <div class="dt_btn_container">
-            <button type="button" class="btn_delete" id="delete">
-                delete
-            </button>
-        </div>
-    </div>`;
+        <div class="popup">
+            <div class="close">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
+                        fill="#0F1729" />
+                </svg>
+            </div>
+            <div class="cp_btn_container"> <button type="button" class="pick_button" id="copy_rgb">copy rgba</button></div>
+            <div class="p_color_box">
+                <span>${rgb}</span>
+            </div>
+            <div class="cp_btn_container"> <button type="button" class="pick_button" id="copy_hsl">copy hsl</button></div>
+            <div class="p_color_box">
+                <span>${hsl}</span>
+            </div>
+            <div class="cp_btn_container"> <button type="button" class="pick_button" id="copy_hex">copy hex</button></div>
+            <div class="p_color_box">
+                <span>${hex}</span>
+            </div>
+            <div class="dt_btn_container">
+                <button type="button" class="btn_delete" id="delete">
+                    delete
+                </button>
+            </div>
+        </div>`;
     document.body.appendChild(div);
     const popup = document.querySelector(".popup");
     const copy_rgb = document.querySelector("#copy_rgb");
     const copy_hsl = document.querySelector("#copy_hsl");
     const copy_hex = document.querySelector("#copy_hex");
 
-    copy_hex.addEventListener("click",(e)=>{
+    copy_hex.addEventListener("click", (e) => {
         const color_code = e.target.parentElement.nextElementSibling.firstElementChild.textContent;
         e.target.textContent = "Copied"
         setTimeout(() => {
@@ -193,18 +195,18 @@ function toggleColorModal(e){
         // e.target.textContent = "copped"
         navigator.clipboard.writeText(color_code);
     });
-    copy_rgb.addEventListener("click",(e)=>{
+    copy_rgb.addEventListener("click", (e) => {
         const color_code = e.target.parentElement.nextElementSibling.firstElementChild.textContent;
         navigator.clipboard.writeText(color_code);
-         e.target.textContent = "Copied"
+        e.target.textContent = "Copied"
         setTimeout(() => {
             e.target.textContent = "copy hex"
         }, 1000);
     });
-    copy_hsl.addEventListener("click",(e)=>{
+    copy_hsl.addEventListener("click", (e) => {
         const color_code = e.target.parentElement.nextElementSibling.firstElementChild.textContent;
         navigator.clipboard.writeText(color_code);
-         e.target.textContent = "Copied"
+        e.target.textContent = "Copied"
         setTimeout(() => {
             e.target.textContent = "copy hex"
         }, 1000);
@@ -212,13 +214,13 @@ function toggleColorModal(e){
 
 
     // for animation
-    setTimeout(()=>{
+    setTimeout(() => {
         popup.style.opacity = "1";
         popup.style.top = "50%";
-    },10);
+    }, 10);
 
     const closeBtn = document.querySelector(".close");
-    closeBtn.addEventListener("click",()=>{
+    closeBtn.addEventListener("click", () => {
         popup.style.opacity = "0";
         popup.style.top = "45%";
         setTimeout(() => {
@@ -228,11 +230,13 @@ function toggleColorModal(e){
     });
 
 
+
+
 }
 
 
 // save color on local storage
-function updateLocalStorage(){
+function updateLocalStorage() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(myColor))
 }
 
